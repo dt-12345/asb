edc672ab41a95a1d5a3123dbeb951bf0affec043
dt-12345 <damagetesting@proton.me>
2024-04-10 18:49:22 -0400

    added oead alternative

--- zstd.py
+++ zstd.py
@@ -5,10 +5,16 @@ except ImportError:
 try:
     import oead
 except ImportError:
-    raise ImportError("oead not found (pip install oead)")
+    print("oead not found (pip install oead)")
+    print("using sarc.py instead")
+    try:
+        import sarc
+    except ImportError:
+        raise ImportError("sarc.py not found")
 from functools import lru_cache
 from pathlib import Path
 from typing import Dict, List
+import sys
 
 class ZstdDecompressor(zstd.ZstdDecompressor):
     def __init__(self, dictionary: zstd.ZstdCompressionDict=None, format: int=zstd.FORMAT_ZSTD1) -> None:
@@ -28,8 +34,12 @@ class ZstdDecompContext:
     @lru_cache
     def __init__(self, zsdic_pack_path: str="") -> None:
         vanilla_decompressor: zstd.ZstdDecompressor = zstd.ZstdDecompressor()
-        archive: oead.Sarc = oead.Sarc(vanilla_decompressor.decompress(Path(zsdic_pack_path).read_bytes()))
-        dictionaries: Dict[str, zstd.ZstdCompressionDict] = {f.name: zstd.ZstdCompressionDict(f.data) for f in archive.get_files()}
+        if "oead" in sys.modules:
+            archive: oead.Sarc = oead.Sarc(vanilla_decompressor.decompress(Path(zsdic_pack_path).read_bytes()))
+            dictionaries: Dict[str, zstd.ZstdCompressionDict] = {f.name: zstd.ZstdCompressionDict(f.data) for f in archive.get_files()}
+        else:
+            archive: sarc.Sarc = sarc.Sarc(vanilla_decompressor.decompress(Path(zsdic_pack_path).read_bytes()))
+            dictionaries: Dict[str, zstd.ZstdCompressionDict] = {i["Name"] : zstd.ZstdCompressionDict(i["Data"]) for i in archive.files}
         self.pack: ZstdDecompressor = ZstdDecompressor(dictionaries["pack.zsdic"])
         self.bcett: ZstdDecompressor = ZstdDecompressor(dictionaries["bcett.byml.zsdic"])
         self.zs: ZstdDecompressor = ZstdDecompressor(dictionaries["zs.zsdic"])
